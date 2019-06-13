ac795d50baf72c8ce9f197abe8379f88a756b16f
Anna W <anski.anna@gmail.com>
2019-06-13 15:29:26 +0100

    function works to add a new friends with saveFriendData method and using a post request

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,77 +1,118 @@
 import React from 'react';
-// import axios from 'axios';
+// import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import Friends from './components/Friends';
+import FriendsForm from './components/FriendsForm';
 
-class Friends extends React.Component {
+const friendsApi = 'http://localhost:5000/friends';
 
-    render(){
-        const { friend } = this.props;
-        console.log(friend);
-        return (
-            //  friends.map(friend => {
-            //      return (
-                     <p>
-                         Name: {friend.name}<br />
-                         Age: {friend.age}<br />
-                         Email: {friend.email}
-                     </p>
-            //      )
-            //  })
-        )
+class App extends React.Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        errorMessage: '',
+        friends: [],
+      }
+    }
 
+    // newAgeInput = React.createRef();
+
+    // idToGetInput = React.createRef();  this.idToGetInput.current.value
+
+    // newNameInput = React.createRef();
+
+    // newAgeInput = React.createRef();
+    
+    // newEmailInput = React.createRef();
+
+    // idToReplaceInput = React.createRef();
+
+    // nameToReplaceInput = React.createRef();
+
+    // ageToReplaceInput = React.createRef();
+
+    // idToDeleteInput = React.createRef();
+
+
+    componentDidMount() {
+      this.getFriends();
+    }
+
+      // crud operations
+    getFriends = () => {
+      axios
+        .get(friendsApi)
+        .then(response => {
+          this.setState({ friends: response.data });
+        })
+        .catch(error => {
+          this.setState({errorMessage: error.message});
+        });
+    };
+
+    saveFriendData = (event) => {
+      event.preventDefault();
+      console.log('submitted');
+      const newFriend = {
+        name: this.state.name, 
+        age: parseInt(this.state.age), 
+        email: this.state.email };
+      axios
+        .post(friendsApi, newFriend)
+        .then(response => 
+          this.setState({ friends: response.data, name: '', email: '', age: '' })
+        );
+    };
+   
+    
+    handleTextInput = event => {
+      event.preventDefault();
+      this.setState({ [event.target.name]: event.target.value });
+    }
+
+  
+    render() {
+      const { name, age, email } = this.state;
+      return (
+        <div className="App">
+         <div >
+            <h1>Friends List</h1>
+            {this.state.friends.map(friend => (
+            <Friends 
+            key={friend.id}
+            handleTextInput={this.handleTextInput}
+            friend={friend} 
+           />
+           )) 
+         }
+         {/* <div>
+          <h5>[POST] a new friend</h5>
+          name: <input type='text' ref={this.newNameInput} /><br />
+          age: <input type='text' ref={this.newAgeInput} /><br />
+          <button onClick={this.postNewFriend}>postNewFriend</button>
+          </div> */}
+         <FriendsForm 
+            name={name}
+            age={age}
+            email={email}
+            handleTextInput={this.handleTextInput}
+            saveFriendData={this.saveFriendData}
+
+
+         />
+          {/* <FriendsForm 
+           postNewFriend={this.postNewFriend}
+           newNameInput={this.newNameInput}
+           newAgeInput={this.newAgeInput}
+           newEmailInput={this.newEmailInput}
+           saveFriendData={this.saveFriendData}
+           /> */}
+          </div>
+        </div>
+       
+      )
     }
 }
 
-//  export default function Friends ( props ) {
-//     const { friend } = this.props;
-//    return (
-//         friends.map(friend => {
-//             return (
-//                 <p>
-//                     Name: {friend.name}<br />
-//                     Age: {friend.age}<br />
-//                     Email: {friend.email}
-//                 </p>
-//             )
-//         })
-//      )
-// }   
-  export default Friends;
-
-    // showUpdateFriend = () => {
-    //     this.setState({ showUpdateFriend: !this.state.showUpdateFriend });
-    // }
-
-    // deleteFriend = (friendId) => {
-    //     axios
-    //         .delete(`http://localhost:5000/friends/${friendId}`)
-    //         .then(response =>{
-    //             this.props.getFriends();
-    //         })
-    //         .catch(error => {
-    //             console.log(error);
-    //         });
-    // };
-
-    // updateFriend = (friendId) => {
-    //     const friend = {};
-    //     if (this.state.name !== '') {
-    //         friend.name = this.state.name;
-    //     }
-    //     if (this.state.email !== '') {
-    //         friend.email = this.state.email;
-    //     }
-    //     axios
-    //         .put(`http://localhost:5000/friends/${friendId}`, friend)
-    //         .then(response => {
-    //             this.setState({ 
-    //                 showUpdateFriend: false, 
-    //                 name: '', 
-    //                 age: '',
-    //                 email: '',
-    //              });
-    //         this.props.getFriends();
-    //         })
-    //         .catch(error => {
-    //             console.log(error)
-    //      });
-    // };
+export default App;
